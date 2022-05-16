9a589f399c5e5484a2c3c6ea3fe3f76d6282b6d8
Ricielli Costa <rici.spfc@gmail.com>
2022-05-16 19:44:45 -0300

    Iniciando exercicios 4.3

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercices3.js
@@ -0,0 +1,21 @@
+let size = 10
+let resultado = size
+for (let index = 1; index < size; index+=1) {
+    resultado *= index
+}
+console.log(resultado);
+
+// Exercício 2
+let word = 'tryber';
+let saida = ""
+for (let character of word) {
+    saida = character + saida
+}
+console.log(saida);
+
+// Exercício 3
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+for (let word of array) {
+    for 
+console.log(word);  
+}
