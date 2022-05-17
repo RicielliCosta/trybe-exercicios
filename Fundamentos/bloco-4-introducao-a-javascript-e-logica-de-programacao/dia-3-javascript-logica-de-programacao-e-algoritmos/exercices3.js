ccb1068e9f4d8bc9c17208dadffc1eb0ade35a31
Ricielli Costa <rici.spfc@gmail.com>
2022-05-17 01:36:18 -0300

    Finalizando exercicios 4.3

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercices3.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercices3.js
@@ -15,7 +15,33 @@ console.log(saida);
 
 // Exercício 3
 let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maior =''
+let menor = array [0]
 for (let word of array) {
-    for 
-console.log(word);  
+    if (word.length > maior.length){
+        maior = word
+    }
+    if (word.length < menor.length){
+        menor = word
+    }
 }
+console.log(maior);
+console.log(menor);
+
+// Exercício 4
+let limite = 50
+let arrayPrimo =[]
+for (let numero = 2;numero <= limite; numero += 1){
+    let primo = true;
+    for (let divisor = 2; divisor < numero; divisor +=1){
+        if (numero % divisor === 0){
+            primo = false;
+            break
+        }
+    }
+    if (primo){
+        arrayPrimo.push(numero)
+    }  
+}
+let maiorPrimo = Math.max.apply(null,arrayPrimo);
+console.log(maiorPrimo);
